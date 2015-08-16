a97f7a02f303924fc24b0a5c1ac55dafa26ec240
Carl Zulauf <carl@linkleaf.com>
2015-08-16 18:22:36 -0500

    Give scheduler more time, cause jruby on travis is slow

--- spec/threasy/schedule_spec.rb
+++ spec/threasy/schedule_spec.rb
@@ -7,48 +7,52 @@ describe "Threasy::Schedule" do
     it "should allow a job to be processed after specified delay" do
       expect(work).to receive(:enqueue).with(job)
       subject.add(job, in: 0.1)
-      sleep 0.2
+      sleep 0.3
     end
 
     it "should allow a job to be processed at the specified time" do
       expect(work).to receive(:enqueue).with(job)
       subject.add(job, at: Time.now + 0.1)
-      sleep 0.2
+      sleep 0.3
     end
 
     it "should allow a job to be repeated at the specified interval" do
       expect(work).to receive(:enqueue).with(job).at_least(:twice)
       subject.add(job, every: 0.1, in: 0.1)
-      sleep 0.3
+      sleep 0.5
     end
 
     it "should allow blocks to be processed on schedule" do
       block_job = ->{ 1 + 1 }
       expect(work).to receive(:enqueue).with(block_job)
       subject.add(in: 0.1, &block_job)
-      sleep 0.2
+      sleep 0.3
     end
 
     it "should allow string expressions to be processed on schedule" do
       expect(work).to receive(:enqueue).with("TestScheduledJob")
       subject.add("TestScheduledJob", in: 0.1)
-      sleep 0.2
+      sleep 0.3
     end
 
     it "should default first run to now + every_interval" do
       expect(work).to receive(:enqueue).with(job).at_least(:twice)
       subject.add(job, every: 0.1)
-      sleep 0.3
+      sleep 0.5
     end
   end
 
   describe "#remove" do
     it "should be possible to remove a job from the schedule" do
-      expect(work).to receive(:enqueue).with(job).at_least(:once).at_most(:twice)
-      entry = subject.add(job, every: 0.2)
+      expect(work).to receive(:enqueue).with(job).at_least(:once)
+
+      entry = subject.add(job, every: 0.1)
       sleep 0.3
+
       entry.remove
-      sleep 0.5
+
+      expect(work).not_to receive(:enqueue)
+      sleep 0.3
     end
   end
 
